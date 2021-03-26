035bb3e77515b3e29ff6f0388e3bdde36e2f69b8
clemenceclaireR <clemence.robin@protonmail.com>
2021-03-26 17:20:51 +0100

    Add scripts for server; refractor minimal pair table loading

---
+++ tools/bash_scripts/clean_dumps.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+find /home/clemence/site/db_backups -mtime +1 -exec rm {} +
---
+++ tools/bash_scripts/dump_db.sh
@@ -0,0 +1,1 @@
+pg_dump --no-password -U clemence vocal_trainer > /home/clemence/site/db_backups/db_$(date +%Y-%m-%d).pgsql
